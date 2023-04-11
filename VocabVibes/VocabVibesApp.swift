e4a56dbb6ce26a374b39d5cf75f015ec4fc27ccf
klimentiuss <klimenko.apple01@icloud.com>
2023-04-11 16:01:10 +0300

    Initial Commit

---
+++ VocabVibes/VocabVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabVibesApp.swift
+//  VocabVibes
+//
+//  Created by Daniil Klimenko on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VocabVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
